efab9a869ed3c772f7082a83449aeec7408b4ed1
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-24 14:50:01 +0300

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,3 +25,26 @@ def filter_datum(
             message,
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor function"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters incoming logs values"""
+
+        record.msg = filter_datum(
+            list(self.fields), self.REDACTION,
+            record.getMessage(), self.SEPARATOR
+        )
+        return super(RedactingFormatter, self).format(record)
